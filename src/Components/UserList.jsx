70a64ac9e46514361e59d8f88fc906a892d12480
Praneeth22 <yennampraneeth@gmail.com>
2025-01-14 23:39:33 -0500

    demonstration of container_presentation_design_pattern

--- src/Components/UserList.jsx
+++ src/Components/UserList.jsx
@@ -1,13 +1,43 @@
-import React from 'react'
-
-function UserList({users,loading,error}) {
-    if(loading) return <div>...Loading</div>
-    if(error) return <div>Something went wrong!</div>
+import PropTypes from "prop-types";
+function UserList({ users, loading, error }) {
+  if (loading) return <div>...Loading</div>;
+  if (error) return <div>Something went wrong!</div>;
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <div className="container">
+        {users.length &&
+          users.map((user) => {
+            return (
+              <div key={user.id}>
+                <img src={user.avatar} alt={user.first_name} />
+                <h3>
+                  {user.first_name} {user.last_name}
+                </h3>
+                <p>{user.email}</p>
+              </div>
+            );
+          })}
+      </div>
+    </>
+  );
 }
 
-export default UserList
+
+// Define the prop types for the user object
+const user = PropTypes.shape({
+  id: PropTypes.number.isRequired,
+  first_name: PropTypes.string.isRequired,
+  last_name: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+});
+
+// Define the prop types for the UserList component
+UserList.propTypes = {
+  users: PropTypes.arrayOf({
+    user,
+  }).isRequired,
+  loading: PropTypes.bool.isRequired,
+  error: PropTypes.bool.isRequired,
+};
+export default UserList;
